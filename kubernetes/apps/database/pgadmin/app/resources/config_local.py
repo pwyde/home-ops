e9c3b9614d562a976b4995a0fefea8f39c7ef6b1
Patrik Wyde <patrik@wyde.se>
2025-03-07 00:03:16 +0100

    feat(pgadmin): enable oauth

---
+++ kubernetes/apps/database/pgadmin/app/resources/config_local.py
@@ -0,0 +1,16 @@
+AUTHENTICATION_SOURCES = ['oauth2', 'internal']
+OAUTH2_AUTO_CREATE_USER = True
+OAUTH2_CONFIG = [{
+    'OAUTH2_NAME': '${SECRET_DOMAIN}',
+    'OAUTH2_DISPLAY_NAME': 'authentik',
+    'OAUTH2_CLIENT_ID': '<Client ID from authentik>',
+    'OAUTH2_CLIENT_SECRET': '<Client secret from authentik>',
+    'OAUTH2_TOKEN_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/token/',
+    'OAUTH2_AUTHORIZATION_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/authorize/',
+    'OAUTH2_API_BASE_URL': 'https://authentik.${SECRET_DOMAIN}/',
+    'OAUTH2_USERINFO_ENDPOINT': 'https://authentik.${SECRET_DOMAIN}/application/o/userinfo/',
+    'OAUTH2_SERVER_METADATA_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/pgadmin/.well-known/openid-configuration',
+    'OAUTH2_SCOPE': 'openid email profile',
+    'OAUTH2_ICON': 'fa-key',
+    'OAUTH2_BUTTON_COLOR': '#e54c3c'
+}]
