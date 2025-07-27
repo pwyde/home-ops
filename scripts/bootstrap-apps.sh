173c8508fd964109bb357d241de09035f51ce8f5
Patrik Wyde <patrik@wyde.se>
2025-07-27 10:23:58 +0200

    chore: update external-dns crd url in bootstrap script

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -116,10 +116,10 @@ function apply_crds() {
     log debug "Applying CRDs"
 
     local -r crds=(
+        # renovate: datasource=github-releases depName=kubernetes-sigs/external-dns
+        https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.18.0/config/crd/standard/dnsendpoints.externaldns.k8s.io.yaml
         # renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
         https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.84.0/stripped-down-crds.yaml
-        # renovate: datasource=github-releases depName=kubernetes-sigs/external-dns
-        https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.18.0/config/crd/standard/dnsendpoint.yaml
     )
 
     for crd in "${crds[@]}"; do
